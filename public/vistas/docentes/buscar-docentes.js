a152e90cad828b7a5b87de06adb749a646f47096
Josselin19 <elizabeth.yemm@gmail.com>
2020-04-01 17:07:15 -0600

    uso de framework para crear interfaz de usuario

--- public/vistas/docentes/buscar-docentes.js
+++ public/vistas/docentes/buscar-docentes.js
@@ -1,57 +1,32 @@
-export function modulo() {
-    var $ = el => document.querySelector(el),
-        frmBuscarDocentes = $("#txtBuscarDocente");
-    frmBuscarDocentes.addEventListener('keyup', e => {
-        traerDatos(frmBuscarDocentes.value);
-    });
-    let modificarDocente = (docente) => {
-        $("#frm-docentes").dataset.accion = 'modificar';
-        $("#frm-docentes").dataset.iddocente = docente.idDocente;
-        $("#txtCodigoDocente").value = docente.codigo;
-        $("#txtNombreDocente").value = docente.nombre;
-        $("#txtDuiDocente").value = docente.dui;
-        $("#txtDireccionDocente").value = docente.direccion;
-        $("#txtTelefonoDocente").value = docente.telefono;
-    };
-    let eliminarDocente = (idDocente) => {
-        fetch(`private/modulos/docentes/procesos.php?proceso=eliminarDocente&docente=${idDocente}`).then(resp => resp.json()).then(resp => {
-            traerDatos('');
-        });
-    };
-    let traerDatos = (valor) => {
-        fetch(`private/modulos/docentes/procesos.php?proceso=buscarDocente&docente=${valor}`).then(resp => resp.json()).then(resp => {
-            let filas = '';
-            resp.forEach(docente => {
-                filas += `
-                    <tr data-iddocente='${docente.idDocente}' data-docente='${JSON.stringify(docente)}'>
-                        <td>${docente.codigo}</td>
-                        <td>${docente.nombre}</td>
-                        <td>${docente.dui}</td>
-                        <td>${docente.direccion}</td>
-                        <td>${docente.telefono}</td>
-                        <td>
-                            <input type="button" class="btn btn-outline-danger text-white" value="del">
-                        </td>
-                    </tr>
-                `;
+var appBuscarDocentes = new Vue({
+    el: '#frm-buscar-docentes',
+    data: {
+        misdocentes: [],
+        valor: ''
+    },
+    methods: {
+        buscarDocente: function () {
+            fetch(`private/modulos/docentes/procesos.php?proceso=buscarDocente&docente=${this.valor}`).then(resp => resp.json()).then(resp => {
+                this.misdocentes = resp;
             });
-            $("#tbl-buscar-docentes > tbody").innerHTML = filas;
-            $("#tbl-buscar-docentes > tbody").addEventListener("click", e => {
-                e.stopPropagation();
-
-                if (e.srcElement.parentNode.dataset.docente == null) {
-                    var mensaje = confirm("¿Esta seguro que desea eliminar este registro?");
-                    if (mensaje){
-                        alert("¡El registro se ha eliminado con exito!");
-                        eliminarDocente(e.srcElement.parentNode.parentNode.dataset.iddocente);
-                    } else {
-                        alert("¡El registro no ha sido eliminado!");
-                    }   
-                } else {
-                    modificarDocente(JSON.parse(e.srcElement.parentNode.dataset.docente));
-                }
-            });
-        });
-    };
-    traerDatos('');
-}
+        },
+        modificarDocente: function (docente) {
+            appdocente.docente = docente;
+            appdocente.docente.accion = 'modificar';
+        },
+        eliminarDocente: function (idDocente) {
+            var mensaje = confirm("¿Esta seguro que desea eliminar este registro?");
+            if(mensaje){
+                alert("¡El registro se ha eliminado con exito!");
+                fetch(`private/modulos/docentes/procesos.php?proceso=eliminarDocente&docente=${idDocente}`).then(resp => resp.json()).then(resp => {
+                this.buscarDocente();
+                });
+            } else {
+                alert("¡El registro no ha sido eliminado!");
+            }
+        }
+    },
+    created: function () {
+        this.buscarDocente();
+    }
+});
